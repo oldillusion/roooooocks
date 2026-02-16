8e848b13bff3b6cd4304268837069df5e1253af5
oldillusion <mccann.graham@gmail.com>
2026-02-16 16:36:23 +0000

    Not sure this is the right thing

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 require "utilities"
 
+local push = require "lib/push"
+
 local assets = require "assets"
 local main_menu = require "main_menu"
 local shaders = require "shaders"
@@ -22,7 +24,9 @@ function love.load()
     main_menu.load(_GAME_STATE, assets)
     game.load(_GAME_STATE, rocks)
     love.graphics.setFont(assets.getAssetData("font"))
-    love.window.setMode(1280, 800, {fullscreen = false, vsync = false})
+    -- love.window.setMode(1280, 800, {fullscreen = false, vsync = false})
+    push:setupScreen(640, 400, 1280, 800, {fullscreen = false, vsync = false})
+
 end
 
 function love.update(dt)
@@ -34,27 +38,28 @@ function love.update(dt)
     elseif _GAME_STATE.currentScene == _SCENE_ENUM.MAIN_MENU then
         main_menu.update(dt)
     elseif _GAME_STATE.currentScene == _SCENE_ENUM.GAME then
-        -- rocks.updateSpawnedRocks(dt)
+        game.update(dt)
     end
 end
 
 function love.draw()
-    -- love.graphics.scale(2, 2)
-    if _GAME_STATE.currentScene == _SCENE_ENUM.SPLASH then
-        splash.draw()
-    elseif _GAME_STATE.currentScene == _SCENE_ENUM.MAIN_MENU then
-        main_menu.draw()
-    elseif _GAME_STATE.currentScene == _SCENE_ENUM.GAME then
-        -- will get to this
-    end
-    -- love.graphics.setBackgroundColor(love.math.colorFromBytes(57, 123, 68))
-    -- for _, rock in ipairs(rocks.getSpawnedRocks()) do
-    --     if rock.collision then
-    --         love.graphics.setShader(shaders.redRockShader)
-    --     end
-    --     love.graphics.draw(assets.getAssetData("stone"), rock.x, rock.y)
-    --     love.graphics.setShader()
-    -- end
-    -- love.graphics.setColor(1, 1, 1)
-    -- love.graphics.circle("line", _DREAMER.x, _DREAMER.y, _DREAMER.radius)
+    push:start()
+        if _GAME_STATE.currentScene == _SCENE_ENUM.SPLASH then
+            splash.draw()
+        elseif _GAME_STATE.currentScene == _SCENE_ENUM.MAIN_MENU then
+            main_menu.draw()
+        elseif _GAME_STATE.currentScene == _SCENE_ENUM.GAME then
+            -- will get to this
+        end
+        -- love.graphics.setBackgroundColor(love.math.colorFromBytes(57, 123, 68))
+        -- for _, rock in ipairs(rocks.getSpawnedRocks()) do
+        --     if rock.collision then
+        --         love.graphics.setShader(shaders.redRockShader)
+        --     end
+        --     love.graphics.draw(assets.getAssetData("stone"), rock.x, rock.y)
+        --     love.graphics.setShader()
+        -- end
+        -- love.graphics.setColor(1, 1, 1)
+        -- love.graphics.circle("line", _DREAMER.x, _DREAMER.y, _DREAMER.radius)
+    push:finish()
 end
